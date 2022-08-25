12d8649d276037af6115f3af082e34225bf60710
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-25 10:45:47 +0530

    REFACTOR: Flip coin multiple times and store heads count and tails count insted of showing message

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,11 +9,18 @@ function headsOrTails () {
 HEADS=0
 TAILS=1
 
-flipCoin=$(headsOrTails)
-if [ $flipCoin -eq $HEADS ]
-then
-	echo "Flip the coin and its heads winner"
-elif [ $flipCoin -eq $TAILS ]
-then
-	echo "Flip the coin and its tails winner"
-fi
+headsCounter=0
+tailsCounter=0
+for (( i=0; i<10; i++ ))
+do
+	flipCoin=$(headsOrTails)
+	if [ $flipCoin -eq $HEADS ]
+	then
+		headsCounter=$(($headsCounter+1))
+	elif [ $flipCoin -eq $TAILS ]
+	then
+		tailsCounter=$(($tailsCounter+1))
+	fi
+done
+
+echo "$headsCounter times heads won and $tailsCounter times tails won"
