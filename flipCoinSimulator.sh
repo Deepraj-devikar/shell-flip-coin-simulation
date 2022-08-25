5040cef710442859e5a0b37cbe5d1c6a91d10155
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-25 10:36:19 +0530

    ADDED: Flip coin and check heads or tails winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Welcome To Flip Coin Simulator Program"
+
+function headsOrTails () {
+	echo $((RANDOM%2))
+}
+
+HEADS=0
+TAILS=1
+
+flipCoin=$(headsOrTails)
+if [ $flipCoin -eq $HEADS ]
+then
+	echo "Flip the coin and its heads winner"
+elif [ $flipCoin -eq $TAILS ]
+then
+	echo "Flip the coin and its tails winner"
+fi
